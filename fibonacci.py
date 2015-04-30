01957ca39afc919b287264d307dbfcf2547a8fa5
robin <robin.wachsmann@tum.de>
2015-04-30 19:15:55 +0200

    working

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,13 @@
 # fibonacci hausaufgabe
 
 def fibonacci():
-
-    
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
 
 
 gen = fibonacci()
 for i in gen:
     print(i)
-    if i > 10000: break
+    if i > 10e3: break
