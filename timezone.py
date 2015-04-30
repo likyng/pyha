87e8184d4c4c1eb7ec5fec6701b13fd474f06c05
robin <robin.wachsmann@tum.de>
2015-05-01 01:17:08 +0200

    working with countries as list

--- timezone.py
+++ timezone.py
@@ -9,16 +9,16 @@ def readCountryMap():
     for country in countries:
         temp = [country[1],country[2]]
         clean_list.append(temp)
-    return dict(clean_list)
+    return clean_list
 
 def numzones_per_country():
     result = {}
     countries = readCountryMap()
     for element in countries:
-        if element in result:
-            result[element] += 1
+        if str(element[0]) in result:
+            result[str(element[0])] += 1
         else:
-           result[element] = 1
+           result[str(element[0])] = 1
          
 
     print(result)
