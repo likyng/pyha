a96b8383677aa4bf17e7d9ffb491cb4acbd4dbac
robin <robin.wachsmann@tum.de>
2015-04-30 17:23:26 +0200

    fertig

--- filter.py
+++ filter.py
@@ -5,3 +5,7 @@ def deduplicate(entries):
     for entry in entries:
         if entry not in new_list:
             new_list.append(entry)
+    return new_list
+    
+test=deduplicate([1,2,3,4,2,5])
+print test
