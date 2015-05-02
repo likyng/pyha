8a0e8ee05cd240d4a3fd27cfee467aa88337ab27
robin <robin.wachsmann@tum.de>
2015-05-02 22:11:32 +0200

    first draft, to be debugged

--- timezone.py
+++ timezone.py
@@ -1,44 +1,66 @@
 # timezone hausaufgabe
 # https://github.com/likyng/pyha/blob/master/timezone.py
 
-def readCountryMap():
+def readFile(filename):
     import csv
 
     # context manager takes care of closing the file regardless of errors.
-    with open("zone.csv", "r") as countryFile:
-        countryData = countryFile.readlines()
-    countries = csv.reader(countryData)
-    clean_list = []    
-    for country in countries:
-        # to skip the first coloumn which contains not needed information
-        temp = [country[1],country[2]]
-        clean_list.append(temp)
-    return clean_list
+    with open(filename, "r") as file:
+        data = file.readlines()
+    return csv.reader(data)
+
 
 def numzones_per_country():
     result = {}
-    countries = readCountryMap()
+    countries = readFile("zone.csv")
     for element in countries:
-        if str(element[0]) in result:
-            result[str(element[0])] += 1
+        if str(element[1]) in result:
+            result[str(element[1])] += 1
         else:
-           result[str(element[0])] = 1
+           result[str(element[1])] = 1
     print(result)
 
 numzones_per_country()
 
+
 def numzones_per_continent():
     result = {}
-    continents = readCountryMap()
+    continents = readFile("zone.csv")
     for element in continents:
-        if str(element[1]).split('/', maxsplit = 1)[0] in result:
-            result[str(element[1]).split('/', maxsplit = 1)[0]] += 1
+        if str(element[2]).split('/', maxsplit = 1)[0] in result:
+            result[str(element[2]).split('/', maxsplit = 1)[0]] += 1
         else:
-            result[str(element[1]).split('/', maxsplit = 1)[0]] = 1
+            result[str(element[2]).split('/', maxsplit = 1)[0]] = 1
     print(result)
 
 numzones_per_continent()
 
+
 # timezone hausaufgabe nr 2 (fortgeschrittene)
-
-
+def zone_countries():
+    import time
+    result = {}
+    timezones = readFile("timezone.csv")
+    zone = readFile("zone.csv")
+    temp = [timezones[0][0], timezones[0][2]]
+#    for i, zone in enumerate(timezones):
+    for region in timezones:
+        if region[2] <= time.time() and region[0] == temp[0]:
+            temp[1] = region[2]
+        elif region[0] == temp[0] and region[2] >= time.time():
+            if str(temp[0]) in result:
+                result[str(region[1])] += ", %s" % (zone[temp[0][1]])
+            else:
+                result[str(region[1])] = zone[temp[0]][1]
+            temp[0] += 1
+        else:
+            temp[1] = region[2]
+            if str(temp[0]) in result:
+                result[str(region[1])] += ", %s" % (zone[temp[0][1]])
+            else:
+                result[str(region[1])] = zone[temp[0]][1]
+            temp[0] += 1
+            
+    print(result)
+    
+zone_countries()
