d6642bd7c3bbccf05357fdb54e1527bf3f8b1f13
Nimar <l.nimar.b@gmail.com>
2015-05-02 16:51:52 +0200

    added timezones per continent counting function

--- timezone.py
+++ timezone.py
@@ -27,3 +27,18 @@ def numzones_per_country():
 
 numzones_per_country()
 
+def numzones_per_continent():
+    result = {}
+    continents = readCountryMap()
+    for element in continents:
+        if str(element[1]).split('/', maxsplit = 1)[0] in result:
+            result[str(element[1]).split('/', maxsplit = 1)[0]] += 1
+        else:
+            result[str(element[1]).split('/', maxsplit = 1)[0]] = 1
+    print(result)
+
+numzones_per_continent()
+
+# timezone hausaufgabe nr 2 (fortgeschrittene)
+
+
