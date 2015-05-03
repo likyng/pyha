759b35275137d658f0c4c352bf3fa3b33ac419db
Nimar <l.nimar.b@gmail.com>
2015-05-03 15:15:31 +0200

    fixed function up - only replace missing id's with country name left to do

--- timezone.py
+++ timezone.py
@@ -44,13 +44,24 @@ def zone_countries():
     import time
     result = {}
     timezones = readFile("timezone.csv")
+    countryMap = readFile("country.csv")
+    continentMap = readFile("zone.csv")
     for element in timezones:
+        # to skip timezoneless entries
+        if element[2] == "":
+            continue
         if str(element[1]) in result:
-            # wenn die ID noch nicht in der liste 
+            # wenn die bloß die ID noch nicht in dem result dict ist, conditions checken & ggf appenden
             if str(element[0]) not in result[str(element[1])] and int(element[2]) <= time.time():
-                result[str(element[1])].append(str(element[0]))
-        else:
-            result[str(element[1])] = [element[0]]
+                if int(element[3]) > time.time():
+                    result[str(element[1])].append(str(element[0]))
+                elif int(element[3]) == 0:
+                    result[str(element[1])].append(str(element[0]))
+        elif float(element[2]) <= time.time():
+            if int(element[3]) > time.time():
+                result[str(element[1])] = [element[0]]
+            elif int(element[3]) == 0:
+                result[str(element[1])] = [element[0]]
     # missing: replace ID's with the corresponding country name found in zone.csv
 
     print(result)
