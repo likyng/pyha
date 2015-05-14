a0133a64c008f0d416ea3ffc9884476f05614e9e
Nimar <l.nimar.b@gmail.com>
2015-05-15 01:14:28 +0200

    more unit tests

--- test_filter.py
+++ test_filter.py
@@ -5,7 +5,27 @@ from filter import deduplicate
 import unittest
 
 class TestDeduplicate(unittest.TestCase):
-    def test_deduplicate(self):
+    def test_deduplicate_numbers(self):
         dedup = deduplicate([1, 1, 2])
         result = [1, 2]
+        self.assertEqual(result, dedup)
+
+    def test_deduplicate_noargs(self):
+        dedup = deduplicate([])
+        result = []
+        self.assertEqual(result, dedup)
+
+    def test_deduplicate_text(self):
+        dedup = deduplicate(['h', 'h', 'g', 2, 2])
+        result = ['h', 'g', 2]
+        self.assertEqual(result, dedup)
+
+    def test_deduplicate_trice(self):
+        dedup = deduplicate(['h', 'h', 'h', 'g', 2, 2])
+        result = ['h', 'g', 2]
+        self.assertEqual(result, dedup)
+
+    def test_deduplicate_neg(self):
+        dedup = deduplicate(['h', 'h', 'h', 'g', -2, -2])
+        result = ['h', 'g', -2]
         self.assertEqual(result, dedup)
